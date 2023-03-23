2956799f9306a8dbd7c21f37320a2d3608019d88
orcakid <orcakid97@gmail.com>
2023-03-23 20:32:32 +0300

    first models

--- src/menu/models.py
+++ src/menu/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    """Класс меню ресторана
+    """
+    #
+    #dish_count
+    title = models.CharField(max_length=30,unique=True)
+    description = models.TextField(blank=True, null=False, default='')
+    submenu_count = models.IntegerField(default=0)
+    
+class Submenu(models.Model):
+    """Класс подменю ресторана
+    """
+    #submenu_count
+    #dish_count
+    title = models.CharField(max_length=30,unique=True)
+    description = models.TextField(blank=True, null=False, default='')
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    
+    
+    
+# class Dish(models.Model):
+#     """Класс блюд ресторана
+#     """
+#     #submenu_count
+#     #dish_count
+#     title = models.CharField(max_length=30,unique=True)
+#     description = models.TextField(blank=True, null=False, default='')
--- src/menu/views.py
+++ src/menu/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Menu, Submenu
 
-# Create your views here.
+
+def home(request):
+    a = Menu.objects.get(pk=1)
+    res = []
+    #Submenu.objects.create(title='Menda',description='gkgskgsg', menu=a)
+    a.submenu_count = len(a.submenu_set.all())
+    return HttpResponse(a.submenu_count)
+
+
